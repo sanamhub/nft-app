c0c410170cf772e709cdc8868a7e6a71f738c43b
Sanam <sanampakuwal27@gmail.com>
2022-04-01 22:45:50 +0545

    init

---
+++ screens/Home.js
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import { View, SafeArea, FlatList, Text } from "react-native";
+
+const Home = () => {
+  return <Text>Hello World!</Text>;
+};
+
+export default Home;
